6c01d211afa7247ba0de23443e46e2fe23781784
ToAndFrom Developer <developer@toandfrom.local>
2025-11-06 16:40:23 +0900

    Local project files - initial setup

---
+++ backend/src/main/java/com/toandfrom/toandfrom/ToandfromApplication.java
@@ -0,0 +1,13 @@
+package com.toandfrom.toandfrom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToandfromApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToandfromApplication.class, args);
+	}
+
+}
