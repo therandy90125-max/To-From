4aa5779f2c781c3ee6b87d4251c239e700919d79
KDH-0309 <donghyun1198@gmail.com>
2025-11-06 15:25:06 +0900

    Initial commit

---
+++ backend/src/test/java/com/toandfrom/toandfrom/ToandfromApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toandfrom.toandfrom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToandfromApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
